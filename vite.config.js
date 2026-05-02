af90ab00f62ea9cde98afeca714ce9de8bdab696
Duc Trung <trunghtmktb@gmail.com>
2026-05-03 01:01:48 +0700

    port

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8080,
-    strictPort: true,
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+    },
   },
 });
